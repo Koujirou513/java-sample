1caf190b63ae95c19d2166ddb3cfffa55dadaa19
Koujirou513 <kojiro323a@gmail.com>
2025-06-01 23:09:31 +0900

    [add] entityに合わせてビールRepositoryの内容追加

--- src/main/java/com/example/myapp/repository/BeerRepository.java
+++ src/main/java/com/example/myapp/repository/BeerRepository.java
@@ -4,7 +4,11 @@ import com.example.myapp.entity.Beer;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface BeerRepository extends JpaRepository<Beer, Long>{
-    
+
+    // 有効なビールを取得
+    List<Beer> findByIsActiveTrueOrderByName();
 }
