25ddb6357bf2caf2bebc72e1b83e7318ec03893b
Koujirou513 <kojiro323a@gmail.com>
2025-06-05 14:45:47 +0900

    [add] DTO作成

---
+++ src/main/java/com/example/myapp/dto/SalesDisplayDto.java
@@ -0,0 +1,22 @@
+package com.example.myapp.dto;
+
+import java.time.LocalDate;
+import java.util.List;
+
+import lombok.Data;
+
+@Data
+public class SalesDisplayDto {
+    private Long id; 
+    private LocalDate targetDate; 
+    private String createdByName;
+    private Integer WeatherCondition;
+    private List<SalesItemDisplayDto> salesItems;
+    private Integer totalQuantity; 
+
+    @Data
+    public static class SalesItemDisplayDto {
+        private String itemName;
+        private Integer quantity;
+    }
+}
---
+++ src/main/java/com/example/myapp/dto/SalesInputDto.java
@@ -0,0 +1,35 @@
+package com.example.myapp.dto;
+
+import java.time.LocalDate;
+import java.util.List;
+
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
+import lombok.Data;
+
+@Data
+public class SalesInputDto {
+    
+    @NotNull(message = "販売日は必須です")
+    private LocalDate targetDate; 
+
+    @NotNull(message = "天気は必須です")
+    @Min(value = 1, message = "天気情報が不正です")
+    @Max(value = 5, message = "天気情報が不正です")
+    private Integer weatherCondition; 
+
+    @Valid
+    private List<SalesItemDto> salesItems;
+
+    @Data
+    public static class SalesItemDto {
+        
+        @NotNull(message = "商品IDは必須です")
+        private Long itemId; // 商品ID
+
+        @NotNull(message = "数量は必須です")
+        private Integer quantity; // 数量
+    }
+}
