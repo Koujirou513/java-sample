8348a87d3bd700be6bea3d7d6c984b2893ed77d0
Koujirou513 <kojiro323a@gmail.com>
2025-06-06 20:33:46 +0900

    [add] グローバル例外ハンドラーを追加

---
+++ src/main/java/com/example/myapp/exception/GlobalExceptionHandler.java
@@ -0,0 +1,16 @@
+package com.example.myapp.exception;
+
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.servlet.ModelAndView;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+    @ExceptionHandler(Exception.class)
+    public ModelAndView handleException(Exception ex, Model model) {
+        ModelAndView mav = new ModelAndView("error");
+        mav.addObject("error", "システムエラーが発生しました" + ex.getMessage());
+        return mav;
+    }
+}
