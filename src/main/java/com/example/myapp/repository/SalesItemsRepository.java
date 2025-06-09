961018b0659904e83b1b4ee9c122c6c7cb426769
Koujirou513 <kojiro323a@gmail.com>
2025-06-09 20:42:57 +0900

    [add] SalesItemsRepositoryに特定の販売IDのアイテムを取得および削除するメソッドを追加

--- src/main/java/com/example/myapp/repository/SalesItemsRepository.java
+++ src/main/java/com/example/myapp/repository/SalesItemsRepository.java
@@ -3,6 +3,7 @@ package com.example.myapp.repository;
 import com.example.myapp.entity.SalesItems;
 
 import java.time.LocalDate;
+import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
@@ -16,4 +17,14 @@ public interface SalesItemsRepository extends JpaRepository<SalesItems, Long> {
      */
     @Query("SELECT COALESCE(SUM(si.quantity), 0) FROM SalesItems si WHERE si.sales.date = :date")
     Integer sumQuantityByDate(LocalDate date);
+
+    /*
+     * 特定の販売IDのアイテムを取得
+     */
+    List<SalesItems> findBySalesId(Long salesId);
+
+    /*
+     * 特定の販売IDのアイテムを削除
+     */
+    void deleteBySalesId(Long salesId);
 }
